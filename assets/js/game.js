eb16907a347df7c6d9f636f19883db96a57e9344
icepants <shawnluther99@gmail.com>
2019-05-15 23:54:24 -0400

    Created an array with four numbers that are randomly selected, created a populator to set the random number to HTML.

---
+++ assets/js/game.js
@@ -0,0 +1,44 @@
+// Run when the DOM is ready.
+$(document).ready(function () {
+
+    // 1.) Grab our DOM values for later population.
+
+    // Global Variables.
+    var wins = 0;
+    var losses = 0;
+    var playerTotalScore = 0;
+
+    // Random Number ID.
+    var randomNumber = $("#randomNumber");
+    // Win Counter ID.
+    var winCounter = $("#winCounter");
+    // Loss Counter ID.
+    var lossCounter = $("#lossCounter");
+    // Player Total Score ID.
+    var playerTotalScore = $("#playerTotalScore");
+
+    // 2.) Generate a random number.
+
+    // Our Static Number Index.
+    var numIndex = ["28", "38", "48", "98"];
+    // Set a variable that holds our random values.
+    var randomNumbz = numIndex[Math.floor(Math.random() * numIndex.length)];
+    // Populate our 'randomNumbz' to the 'randomNumber' display in HTML.
+    randomNumber.text(randomNumbz);
+
+
+    // 3.) When the player clicks on any of the four crystals, increment the 'playerTotalScore'.
+
+    
+
+
+
+
+    // 4.) If the value of the players total score matches the 'randomNumber' displayed, add a win.
+    // 4.2 Otherwise, if the value surpasses the limit of the 'randomNumber', add a loss.
+
+
+
+
+
+});
