22a6288c0a3a44e144c510dd96cf9915cc1acfec
icepants <shawnluther99@gmail.com>
2019-05-17 21:09:06 -0400

    Added Wins/Losses to the game!

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,30 +6,16 @@ $(document).ready(function () {
 
     // Declare Our Game Stats.
     var wins = 0;
-    var losses = 0;
+    var losses = 1;
     var playerTotalScore = 0;
 
-    // Holds our random number for player to guess.
+    // Holds the random number for player to guess.
     var randomNumbz = Math.floor(Math.random() * 120) + 19;
-
-    // Random Crystal Values.
-    var redCrystalValue = Math.floor(Math.random() * 18) + 1;
-    var blueCrystalValue = Math.floor(Math.random() * 12) + 1;
-    var yellowCrystalValue = Math.floor(Math.random() * 12) + 1;
-    var greenCrystalValue = Math.floor(Math.random() * 12) + 1;
-
-
 
     // DOM Values (For later population).
 
     // Random Number ID.
     var randomNumber = $("#randomNumber");
-    // Win Counter ID.
-    var winCounter = $("#winCounter");
-    // Loss Counter ID.
-    var lossCounter = $("#lossCounter");
-    // Player Total Score ID.
-    var totalPlayerScore = $("#totalPlayerScore");
 
     // 2.) Generate a random number.
 
@@ -38,24 +24,28 @@ $(document).ready(function () {
 
     // 3.) When the player clicks on any of the four crystals, increment the 'playerTotalScore'.
 
-    // Generate a random value for each crystal.
-
-    // Total Points.
-    var theTotal = 0;
-    // When the player clicks a button, read each value and output the total points.
+    // Crystal Buttons.
     $('button').click(function () {
-        theTotal = Number(theTotal) + Number($(this).val());
-        $('#totalPlayerScore').text("Total: " + theTotal);
+        // Update our total var to equal our new value.
+        playerTotalScore = Number(playerTotalScore) + Number($(this).val());
+
+        // 4.) If the value of the players total score matches the 'randomNumber' displayed, add a win.
+        if (randomNumbz === playerTotalScore) {
+            wins++;
+            $('#winCounter').text(wins);
+            playerTotalScore = 0;
+
+            // 4.2 Otherwise, if the value surpasses the limit of the 'randomNumber', add a loss.
+        } else if (playerTotalScore > randomNumbz) {
+            $('#lossCounter').text(losses);
+            losses++;
+            playerTotalScore = 0;
+        }
+        // Populate page with total score.
+        $('#totalPlayerScore').text(playerTotalScore);
     });
 
     // Populates the total from the four crystals.
-    $('#totalPlayerScore').text("Total: " + theTotal);
-
-    // 4.) If the value of the players total score matches the 'randomNumber' displayed, add a win.
-    // 4.2 Otherwise, if the value surpasses the limit of the 'randomNumber', add a loss.
-
-
-
-
-
+    $('#totalPlayerScore').text(playerTotalScore);
+    playerTotalScore = Number(playerTotalScore) + Number($(this).val());
 });
